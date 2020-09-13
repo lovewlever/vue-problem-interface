bfe7d7c441f18c3aa4957dc4b05efaed77c978a2
lovew <lovewlever@outlook.com>
2020-09-13 22:52:37 +0800

    WebSocket

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,33 +1,29 @@
 <template>
-    <div style="display: flex;width: 100%;height: 100%">
-        <div style="margin: auto">
-            <div class="spinner-border text-primary" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-            <div class="spinner-border text-secondary" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-            <div class="spinner-border text-success" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-            <div class="spinner-border text-danger" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-            <div class="spinner-border text-warning" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-            <div class="spinner-border text-info" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-            <div class="spinner-border text-light" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-            <div class="spinner-border text-dark" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-        </div>
+  <div style="display: flex;width: 100%;height: 100%">
+    <div style="margin: auto">
+      <div class="spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+      <div class="spinner-border text-secondary" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+      <div class="spinner-border text-success" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+      <div class="spinner-border text-danger" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+      <div class="spinner-border text-warning" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+      <div class="spinner-border text-info" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+      <div class="spinner-border text-dark" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
     </div>
-
+  </div>
 </template>
 
 <script>
@@ -40,41 +36,43 @@ import FuncCommon from "../constants/FuncCommon";
 
 export default {
   name: "Welcome",
-    created() {
-      const timer = setInterval(() => {
-          this.firstVerifyToken()
-          clearInterval(timer)
-      },2000)
-
-        window.Notification.requestPermission();
-    },
-    methods: {
-      firstVerifyToken() {
-          let _this = this
-          let info = FuncCommon.getStorageLoginInfo()
-          console.info(info)
-          if (info === null) {
-              this.$router.push("/login")
-          } else {
-              console.info(info)
-              $.ajax({
-                  url: ConstWeb.WebApi.UNIVERSAL_VERIFY_TOKEN,
-                  headers: {
-                      "token":info.token
-                  },
-                  data:{},
-                  success:function (data) {
-                      console.info(data)
-                      if (data.code === 200) {
-                          _this.$router.push("/home")
-                      } else {
-                          _this.$router.push("/login")
-                      }
-                  }
-              })
+  created() {
+    const timer = setInterval(() => {
+      this.firstVerifyToken();
+      clearInterval(timer);
+    }, 2000);
+  },
+  methods: {
+    firstVerifyToken() {
+      let _this = this;
+      let info = FuncCommon.getStorageLoginInfo();
+      FuncCommon.showConsoleInfo(info);
+      if (info === null) {
+        this.$router.push("/login");
+      } else {
+        FuncCommon.showConsoleInfo(info);
+        $.ajax({
+          url: ConstWeb.WebApi.UNIVERSAL_VERIFY_TOKEN,
+          headers: {
+            token: info.token
+          },
+          data: {},
+          success: function(data) {
+            FuncCommon.showConsoleInfo(data);
+            if (data.code === 200) {
+              _this.$router.push("/home");
+            } else {
+              _this.$router.push("/login");
+            }
+          },
+          error: function(e) {
+            FuncCommon.showConsoleError(e);
+            _this.$router.push("/login");
           }
+        });
       }
     }
+  }
 };
 </script>
 
